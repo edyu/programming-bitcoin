b7795bd1cfdeb1b78ff013df1ea4d8b5350fe5a1
Ed Yu <edyu@users.noreply.github.com>
2026-03-11 19:46:08 -0700

    first commit

---
+++ src/App/Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+open Library
+
+[<EntryPoint>]
+let main args =
+    let field = [ 0 .. 18 ]
+    let ks = [ 1; 3; 7; 13; 18 ]
+    let fields = [ for k in ks -> List.map (fun x -> (x * k) % 19) field ]
+
+    for f in fields do
+        printfn "%A" (List.sort f)
+
+    let field2 = [ 0 .. 19 ]
+    let ks2 = [ 1; 3; 7; 13; 18; 6; 4; 10 ]
+    let fields2 = [ for k in ks2 -> List.map (fun x -> (x * k) % 20) field2 ]
+
+    for f in fields2 do
+        printfn "%A" (List.sort f)
+
+    let a = FieldElement.Create 7 13 
+    printfn "%O" a
+    let b = FieldElement.Create 12 13
+    let c = FieldElement.Create 6 13
+    let d = FieldElement.Create 3 13
+    let e = FieldElement.Create 10 13
+    let f = FieldElement.Create 1 13
+    printfn "%A" b
+    printfn "a==b=%b" (a=b)
+    printfn "a==a=%b" (a=a)
+    printfn "num=%d, prime=%d" a.Num a.Prime
+    printfn "%A + %A == %A: %b" a b c (a + b = c)
+    printfn "%A - %A == %A: %b" d c e (d - c = e)
+    printfn "%A * %A == %A: %b" d b e (d * b = e)
+    printfn "%A ** 3 == %A: %b" d f ((d *^ 3) = f)
+    printfn "%A ** 12 == %A" d (d *^ 12)
+    printfn "%A ** 12 == %A" e (e *^ 12)
+    printfn "%A ** -3 = %A == %A ** 9 = %A: %b" c (c *^ -3) c (c *^ 9) (c *^ -3 = c *^ 9)
+
+    // let c = FieldElement.Create -2 17
+    // let d  = FieldElement.Create 19 17
+
+    0 // return an integer exit code
---
+++ src/Library/Library.fs
@@ -0,0 +1,30 @@
+﻿module Library
+
+//module ecc
+
+type FieldElement = private { num: int; prime: int } with
+    member this.Num = this.num
+    member this.Prime = this.prime
+    override this.ToString() = $"FieldElement_{this.Prime}({this.Num})"
+    static member Create Num Prime =
+        if Num >= Prime || Num  < 0 then
+            invalidArg "Num" $"Num {Num} not in field range 0 to {Prime-1}"
+        else
+            { num = Num; prime = Prime }
+    static member (+) (a, b: FieldElement) =
+        if a.prime <> b.prime then
+            failwith "Cannot add two numbers in different Fields" 
+        { num = (a.num + b.num) % a.prime; prime = a.prime }
+    static member (-) (a, b: FieldElement) =
+        if a.prime <> b.prime then
+            failwith "Cannot subtract two numbers in different Fields" 
+        let d = (a.num - b.num + a.prime) % a.prime
+        { num = d; prime = a.prime }
+    static member (*) (a, b: FieldElement) =
+        if a.prime <> b.prime then
+            failwith "Cannot multiply two numbers in different Fields" 
+        { num = (a.num * b.num) % a.prime; prime = a.prime }
+    static member ( *^ ) (a: FieldElement, e) =
+        let n = (e % (a.prime - 1) + (a.prime - 1)) % (a.prime - 1) 
+        let nn = (pown a.num n) % a.prime
+        { num = nn; prime = a.prime }
